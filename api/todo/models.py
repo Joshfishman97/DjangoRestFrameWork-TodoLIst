14650689f42f9f5640b84c32a84b9f822ec89828
Joshfishman97 <fishmanjosh251@gmail.com>
2021-11-08 17:23:26 +0000

    fixed class todo by changing it Todo to fit conventions

--- api/todo/models.py
+++ api/todo/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class todo(models.Model):
+class Todo(models.Model):
     title = models.CharField(max_length = 100)
     description =  models.CharField(max_length = 100)
     completed = models.BooleanField(default=False)
