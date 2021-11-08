c1fe1175e13452e9eb7af15ff6a17b5f0e4ef062
Joshfishman97 <fishmanjosh251@gmail.com>
2021-11-08 17:59:19 +0000

    worked on views and created urls.py in the views i made a query for all objects with todo

--- api/todo/views.py
+++ api/todo/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from models import Todo
+from serializers import TodoSerializer
 # Create your views here.
+
+class TodiViewset(viewsets.ModelViewset):
+    #use all the objects that are Todo
+    queryset = Todo.objects.all()
+    serializers_class = TodoSerializer
